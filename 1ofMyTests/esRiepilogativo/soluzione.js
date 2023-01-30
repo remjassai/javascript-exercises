1fcaeb492fde804c4e363d2987512cb33fdc0eca
Rem Jassai <60391009+remjassai@users.noreply.github.com>
2023-01-30 18:06:45 +0100

    fixed the youngest function

--- 1ofMyTests/esRiepilogativo/soluzione.js
+++ 1ofMyTests/esRiepilogativo/soluzione.js
@@ -1,13 +1,7 @@
 const students = [
   { id: 1, name: "Luca", surname: "Rossi", age: 20, hobby: "gamer" },
   { id: 2, name: "Mario", surname: "Bianchi", age: 24, hobby: "sportsman" },
-  {
-    id: 5,
-    name: "Elena",
-    surname: "Pappalardo",
-    age: 23,
-    hobby: "photographer",
-  },
+  {id: 5, name: "Elena", surname: "Pappalardo", age: 23, hobby: "photographer",},
   { id: 3, name: "Giovanna", surname: "Brambilla", age: 26, hobby: "gamer" },
   { id: 4, name: "Carmelo", surname: "Verdi", age: 18, hobby: "photographer" },
 ];
--- 1ofMyTests/esRiepilogativo/test.js
+++ 1ofMyTests/esRiepilogativo/test.js
@@ -1,47 +1,42 @@
 const students = [
     { id: 1, name: "Luca", surname: "Rossi", age: 20, hobby: 'gamer'},
-    { id: 2, name: "Mario", surname: "Bianchi", age: 26, hobby: 'sportsman'},
+    { id: 2, name: "Mario", surname: "Bianchi", age: 24, hobby: 'sportsman'},
     { id: 3, name: "Giovanna", surname: "Brambilla", age: 26, hobby: 'gamer'},
     { id: 4, name: "Carmelo", surname: "Verdi", age: 18, hobby: 'photographer'},
     ];
 
+    //I'm adding a new student in the third place of the array
     students.splice(2, 0,{ id: 5, name: "Elena", surname: "Pappalardo", age: 23, hobby: 'photographer'});
-
     
-    for(i = 0; i < students.length; i++){
+    for(let i = 0; i < students.length; i++){
+        console.log(students[i].name);
         let hobby = students[i].hobby;
         switch (hobby) {
             case "gamer":
-                console.log(students[i].name);
                 console.log("Life Is a Game, Play to Win!");
                 break;
             case "sportsman":
-                console.log(students[i].name);
                 console.log("Never give up!");
                 break;
             case "photographer":
-                console.log(students[i].name);
                 console.log("A picture is worth a thousand words!");
                 break;
         }
     }
 
+    let younger = students[0];
 
+    function youngest(stud){
+        if(younger.age > stud.age) {
+            younger = stud;        
+        }
+    }
 
-    function youngest(){
-        
         for(let i=0; i < students.length; i++){
-            let age = students[0].age;
-            let name = students[0].age;
-
-            if(students[i].age < age){
-                name = students[i].name;
-                return name;
-            }
-
+            youngest(students[i]);
         }
   
-    }
+
 
     function longerSurname(){
         let i = 0;
@@ -54,11 +49,7 @@ const students = [
             i++
         }
           return long; 
-    }
-
-
-    
-        
-    console.log("Il più giovane è " + youngest() + "\n" + "Il cognome più lungo è " + longerSurname());
+    }      
+    console.log("Il più giovane è " + younger.name + "\n" + "Il cognome più lungo è " + longerSurname());
 
 // console.table(students);
